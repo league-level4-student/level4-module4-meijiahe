25e376004ad0f5ed6119f9a3e88fcc5ffd63e71e
meijiahe <meijiahe@localhost>
2021-09-05 12:57:53 -0700

    Almost finished the intro to encapsulation

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -19,15 +19,60 @@ package _01_introduction_to_encapsulation;
 public class EncapsulateTheData {
 	//1. Encapsulate the member variables.
 	//   Add restrictions to the setters according to the comment.
-	
+	//the get method returns the variable value, and the set method sets the value.//
+	//getter and setter method name should be matched with their variable//
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
-	
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
+	public void setItemsReceived(int newitemsReceived) {
+		if (itemsReceived>0) {
+			this.itemsReceived=newitemsReceived;
+		}
+		else {
+			this.itemsReceived=0;
+		}
+	}
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+	public void setDegreesTurned(float newdegreesTurned) {
+		degreesTurned=newdegreesTurned;
+		if (degreesTurned<0) {
+			degreesTurned=0;
+		}
+		if (degreesTurned>360) {
+			degreesTurned=360;
+		}
+	}
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	public String getNomenClature() {
+		return nomenclature;
+	}
+	public void setNomenClature(String newnomenclature) {
+		if (nomenclature.equals("")) {
+			nomenclature.equals(" ");
+		}
+		else {
+			nomenclature.equals(newnomenclature);
+		}
+	}
+	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	public Object getMemberObj() {
+		return memberObj;
+	}
+	public void setMemberObj(Object newmemberObj) {
+		if (memberObj instanceof String) {
+			memberObj=new Object();
+		}
+		else {
+			memberObj=newmemberObj;
+		}
+	}
 	public static void main(String[] args) {
 		
 	}
