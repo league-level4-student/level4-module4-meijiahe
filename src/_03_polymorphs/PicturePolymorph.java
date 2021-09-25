c6869058aec31e851d29dcb58f6ed76b52eb3c8d
Meijia He <hemeijia0602@gmail.com>
2021-09-25 11:39:02 -0700

    Finished Polymorphs

--- src/_03_polymorphs/PicturePolymorph.java
+++ src/_03_polymorphs/PicturePolymorph.java
@@ -6,10 +6,13 @@ import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
+import javax.imageio.ImageIO;
+
 public class PicturePolymorph extends Polymorph implements ImageObserver{
 	BufferedImage image;
 	PicturePolymorph(int x, int y) {
 		super(x, y);
+		loadImage("Apple.png");
 		// TODO Auto-generated constructor stub
 	}
 
@@ -17,9 +20,19 @@ public class PicturePolymorph extends Polymorph implements ImageObserver{
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
 		
-		g.drawImage(i, 120,100,this);
+		g.drawImage(image, x, y, width, height, null);
 	}
 
+	void loadImage(String imageFile) {
+	    
+	        try {
+	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+		
+	        } catch (Exception e) {
+	            
+	        }
+	    }
+	
 	@Override
 	public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
 		// TODO Auto-generated method stub
