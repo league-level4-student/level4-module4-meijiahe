dc846b4231180c07aaacceea5846041747317945
Meijia He <hemeijia0602@gmail.com>
2021-09-25 13:00:32 -0700

    Working on the Hospital project

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -1,5 +1,17 @@
 package _04_hospital;
 
-public class Hospital {
+import java.util.ArrayList;
 
+public class Hospital {
+ ArrayList<Doctor> docs = new ArrayList<Doctor>();
+ public ArrayList <Doctor> getDoctors() {
+		return docs;
+	}
+ //void because we are not returning anything//
+ //inside the parentheses is that we are asking for this variable//
+ public void addDoctor(Doctor d) {
+	 
+ }
+ 
+ 
 }
