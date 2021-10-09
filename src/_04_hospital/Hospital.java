132c46c496678491c01e85fd56af4e8f88293fe1
Meijia He <hemeijia0602@gmail.com>
2021-10-09 12:58:19 -0700

    Hospital code working in progress

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -6,12 +6,26 @@ public class Hospital {
  ArrayList<Doctor> docs = new ArrayList<Doctor>();
  public ArrayList <Doctor> getDoctors() {
 		return docs;
+		
 	}
  //void because we are not returning anything//
  //inside the parentheses is that we are asking for this variable//
  public void addDoctor(Doctor d) {
 	 
  }
- 
- 
+public static void main(String[] args) {
+	int a =1000;
+	int b =10;
+	int c = 1000/10;
+	System.out.println(c);
+}
+ public void addPatient (Patient Patient) {
+	  
+  }
+ ArrayList<Patient> patients = new ArrayList<Patient>();
+ public ArrayList <Patient> getPatients() {
+		return patients;
+		
+	}
+  
 }
